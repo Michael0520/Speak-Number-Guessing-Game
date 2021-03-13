2ed12d593218a8223b61ba34daefb5366a91824e
MichaelLuo520 <po2319987@gmail.com>
2021-03-14 06:35:40 +0800

    push the function to check Number

---
+++ all.js
@@ -0,0 +1,65 @@
+const msgEl = document.getElementById("msg");
+const randomNum = getRandomNumber();
+
+console.log("Number:", randomNum);
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+// Start recognition and game
+recognition.start();
+
+// Capture user speak
+function onSpeak(e) {
+  const msg = e.results[0][0].transcript;
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+// Write what user speak
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div>You said:</div>
+    <span class="box"> ${msg}</span>
+    `;
+}
+
+// Check msg against number
+function checkNumber() {
+  const num = +msg;
+
+  // Check if valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += `<div>That is not a valid number</div>`;
+    return;
+  }
+
+  // Check in range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML = `<div>Number must be between 1 and 100</div>`;
+    return;
+  }
+
+  // Check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+      <h2>Congrats! You have guessed the number! <br><br>
+      It was ${num}</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+      `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML = `<div>GO LOWER</div>`;
+  } else {
+    msgEl.innerHTML = `<div>GO HIGHER</div>`;
+  }
+}
+
+// Generate random number
+function getRandomNumber() {
+  // 產生的數會是 9-99，所以 +1，變成隨機產生 1~100 的數字。
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+// Speak result
+recognition.addEventListener("result", onSpeak);
